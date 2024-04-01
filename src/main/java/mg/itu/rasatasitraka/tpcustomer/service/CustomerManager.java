d5c427c1e418a6e0c2211b7730e549df986d217b
SitrakaRasata45 <rasatasitraka2@gmail.com>
2024-04-01 15:55:26 +0300

    Ajout CustomerManager

---
+++ src/main/java/mg/itu/rasatasitraka/tpcustomer/service/CustomerManager.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.rasatasitraka.tpcustomer.service;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import mg.itu.rasatasitraka.tpcustomer.entity.Customer;
+
+/**
+ * Gère les opérations liées à l'entité Customer.
+ *
+ * @author Sitraka Rasata
+ */
+@RequestScoped
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    /**
+     * Récupère tous les clients.
+     *
+     * @return une liste de tous les clients enregistrés.
+     */
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
+    }
+
+    /**
+     * Met à jour un client existant.
+     *
+     * @param customer le client à mettre à jour.
+     * @return le client mis à jour.
+     */
+    @Transactional
+    public Customer update(Customer customer) {
+        return em.merge(customer);
+    }
+
+    /**
+     * Persiste un nouveau client.
+     *
+     * @param customer le nouveau client à persister.
+     */
+    @Transactional
+    public void persist(Customer customer) {
+        em.persist(customer);
+    }
+}
